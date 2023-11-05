ac6133cced80141e2cfda463f778a69fa1cf999d
phananhh003 <phananhh2003@gmail.com>
2023-11-05 12:16:09 +0700

    dmDucAnh

---
+++ DuyAnh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestBaiTapLonWinform2
+{
+    public partial class DuyAnh : Form
+    {
+        public DuyAnh()
+        {
+            InitializeComponent();
+        }
+    }
+}
